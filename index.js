764e7833f1a77b4c31f69bf8421e5a4147930799
Sehrish khan <sehrishahsan1121@gmail.com>
2024-06-06 12:23:58 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// declair a const answer assingbit to the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: " Enter your sentence to count the word:"
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+//print the array of the words to the console
+console.log(words);
+//print the word count of the sentence to the consol
+console.log(`Your sentence word count is ${words.length}`);
